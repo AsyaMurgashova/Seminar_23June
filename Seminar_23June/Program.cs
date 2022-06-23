521ed5124335d7fe0256c5f9d50b4d07870fe5d7
Asya <fcz777@mail.ru>
2022-06-23 22:07:11 +0300

    Third programm added

--- Seminar_23June/Program.cs
+++ Seminar_23June/Program.cs
@@ -8,11 +8,33 @@
 
 
 // Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
-Console.Write("Input first number: ");
-int number_1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second number: ");
-int number_2 = Convert.ToInt32(Console.ReadLine());
-if (number_1 == (number_2 * number_2))
-Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + "yes");
-else
-Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + "no");
+// Console.Write("Input first number: ");
+// int number_1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int number_2 = Convert.ToInt32(Console.ReadLine());
+// if (number_1 == (number_2 * number_2))
+// Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + "yes");
+// else
+// Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + "no");
+
+
+
+
+Console.Write("введите день: ");
+    int day = Convert.ToInt32(Console.ReadLine());
+    if (day == 1)
+      Console.WriteLine("Monday");
+    else if (day == 2)
+      Console.WriteLine("Tessday");
+    else if (day == 3)
+      Console.WriteLine("Wednesday");
+    else if (day == 4)
+      Console.WriteLine("Thursday");
+     else if (day == 5)
+      Console.WriteLine("Friday");
+    else if (day == 6)
+      Console.WriteLine("Saturday");
+    else if (day == 7)
+      Console.WriteLine("Sunday");
+    else
+      Console.WriteLine("Incorrect day");
