803c87f9ed3ca4a38813f54d023249638d29625e
Asya <fcz777@mail.ru>
2022-06-23 21:27:16 +0300

    First programm added

--- Seminar_23June/Program.cs
+++ Seminar_23June/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+// ПРосим у пользователя ввести число, затем возводим его в квадрат и выводим его значение.
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int q = number * number;
+Console.WriteLine(number + " -> " + q);
+
